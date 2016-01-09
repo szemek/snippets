523123cee672ccff0a012cfb9e9d309062a0c560
Przemysław Dąbek <373530+szemek@users.noreply.github.com>
2016-01-09 18:02:24 +0100

    updated count_records.rb

--- ruby/count_records.rb
+++ ruby/count_records.rb
@@ -1,5 +1,4 @@
-# count records in PostgreSQL via ActiveRecord
+# count records via ActiveRecord
 connection = ActiveRecord::Base.connection
-query = "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'"
-tables = connection.execute(query).values.flatten
+tables = connection.tables
 tables.map { |name| connection.execute("SELECT COUNT(*) FROM #{name}").values.flatten.first.to_i }.reduce(:+)
